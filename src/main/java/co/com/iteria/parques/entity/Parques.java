5d1a77c25c7db16686fbbc73d0fbdee4256a8ead
Yoimar Angulo <joimar.angulo.com.co>
2018-08-31 08:37:05 -0500

    Estructura back

---
+++ src/main/java/co/com/iteria/parques/entity/Parques.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.iteria.parques.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Usuario1
+ */
+@Entity
+@Table(name = "PARQUES")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Parques.findAll", query = "SELECT p FROM Parques p"),
+    @NamedQuery(name = "Parques.findById", query = "SELECT p FROM Parques p WHERE p.id = :id"),
+    @NamedQuery(name = "Parques.findByName", query = "SELECT p FROM Parques p WHERE p.name = :name"),
+    @NamedQuery(name = "Parques.findByState", query = "SELECT p FROM Parques p WHERE p.state = :state"),
+    @NamedQuery(name = "Parques.findByCapacity", query = "SELECT p FROM Parques p WHERE p.capacity = :capacity"),
+    @NamedQuery(name = "Parques.findByStatus", query = "SELECT p FROM Parques p WHERE p.status = :status")})
+public class Parques implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Long id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 200)
+    @Column(name = "NAME")
+    private String name;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 200)
+    @Column(name = "STATE")
+    private String state;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "CAPACITY")
+    private int capacity;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "STATUS")
+    private String status;
+
+    public Parques() {
+    }
+
+    public Parques(Long id) {
+        this.id = id;
+    }
+
+    public Parques(Long id, String name, String state, int capacity, String status) {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+        this.capacity = capacity;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Parques)) {
+            return false;
+        }
+        Parques other = (Parques) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "co.com.iteria.parques.entity.Parques[ id=" + id + " ]";
+    }
+    
+}
---
+++ target/generated-sources/annotations/co/com/iteria/parques/entity/Parques_.java
@@ -0,0 +1,17 @@
+package co.com.iteria.parques.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-31T07:58:09")
+@StaticMetamodel(Parques.class)
+public class Parques_ { 
+
+    public static volatile SingularAttribute<Parques, String> name;
+    public static volatile SingularAttribute<Parques, Long> id;
+    public static volatile SingularAttribute<Parques, String> state;
+    public static volatile SingularAttribute<Parques, Integer> capacity;
+    public static volatile SingularAttribute<Parques, String> status;
+
+}
